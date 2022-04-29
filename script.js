a6cdfad49311e89e1a597d6e308b98d1307224bd
KjellKodeHode <kjell.arne.kodehode@gmail.com>
2022-04-29 09:05:52 +0200

    style adjust

---
+++ script.js
@@ -0,0 +1,29 @@
+// input-amount 
+// * 
+// btn-five||btn-ten||btn-fifteen||btn-twenty||btn-fifty||btn-custom 
+// /
+// input-people
+// = tip-prs
+
+// input-amount 
+// * 
+// btn-five||btn-ten||btn-fifteen||btn-twenty||btn-fifty||btn-custom 
+// /
+// input-people
+// = tip
+
+// let bill = document.getElementById("input-amount")
+
+// let tip = document.querySelector("button").addEventListener("click", () =>{
+    
+// })
+
+
+// let persons = document.getElementById("input-people")
+// let tipEach = (bill * tip) / persons
+// let tipTotal = (bill * tip)
+
+
+// document.querySelector('button').addEventListener('click', () =>{
+//     addToDo();
+// })
